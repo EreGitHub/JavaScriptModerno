04089d0e3fe23d081941e899b44d474269fc3462
dev <46026488+EreGitHub@users.noreply.github.com>
2022-06-17 22:45:34 -0400

    Introducción a la Consola

--- Fundamentos/app.js
+++ Fundamentos/app.js
@@ -1,1 +1,22 @@
-console.log("hola mundo")
+//console.log("hola mundo")
+let a = 10;
+var b = 20;
+const c = 30;
+
+//c = 20 //NOTE error por que es una constante 
+
+//diferentes formas de crear variables
+let a1 = 10;
+let b1 = 10;
+let c1 = 10;
+let d1 = a + b;
+
+let a2 = 10,
+    b2 = 'Hola',
+    c2 = 'SpiderMan',
+    d2 = a + b;
+
+const saludo = c + d;
+
+console.log({ a1, b1, c1, d1 });
+console.table({ a1, b1, c1, d1 });
