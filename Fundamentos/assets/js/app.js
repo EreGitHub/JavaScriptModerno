4380702693a248d334b1aefbeca614555733dd86
dev <46026488+EreGitHub@users.noreply.github.com>
2022-06-17 23:22:19 -0400

    Principal Problema con inicialización de variables con VAR

--- Fundamentos/assets/js/app.js
+++ Fundamentos/assets/js/app.js
@@ -1,10 +1,2 @@
-alert("hola desde app.js")
-let a2 = 10,
-    b2 = 'Hola',
-    c2 = 'SpiderMan',
-    d2 = a + b;
-
-const saludo = c + d;
-c = "hola de nuevo";
-
-var miNombre = "pepe";
+var outerWidth = 1000000;
+let nombre = 'pepe';
