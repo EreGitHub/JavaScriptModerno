d738b19785478cc614b9b63df384fe145958d06c
dev <46026488+EreGitHub@users.noreply.github.com>
2022-06-18 23:16:27 -0400

    Palabras reservadas y nombre de variables

---
+++ Fundamentos/assets/js/palabras-reservadas.js
@@ -0,0 +1,8 @@
+//let const = 123;//NOTE const es una palabra reservada
+//let 1nombre='pepe'//NOTE no inicir con numeros(error)
+let objeto123 = 'hola' //NOTE valido
+let _ = 5; //NOTE valido
+let $objeto = 6; //NOTE valido
+//NOTE sentido semantico
+let x = 'pepe';//NOTE no tiene unsentido semantico
+let nombreJugador = 'pepe';//NOTE tiene sentido semantico
