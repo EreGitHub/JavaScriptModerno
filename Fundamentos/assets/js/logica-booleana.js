4410c3a3b1d343062f7c008e1970508bf3454069
dev <46026488+EreGitHub@users.noreply.github.com>
2022-06-21 01:50:21 -0400

    Pro tip: Asignaciones con operadores

--- Fundamentos/assets/js/logica-booleana.js
+++ Fundamentos/assets/js/logica-booleana.js
@@ -2,31 +2,29 @@ const regresaTrue = () => {
     console.log('regresa true');
     return true;
 }
-const regresaFalse = () => {
-    console.log('regresa false');
-    return false;
-}
 
-console.log('not o la negacion');
-console.log(true);//true
-console.log(!true);//false
-console.log(!false);//true
+console.log('Asignaciones');
+const soyUndefined = undefined;
+const soyNull = null;
+const soyFalso = false;
 
-console.log(!regresaFalse());//true
-console.log('AND')
-console.log(true && true);//true
-console.log(true && false);//false
-console.log(true && !false);//true
-console.log(regresaFalse() && regresaTrue());//false
-console.log(regresaTrue() && regresaFalse());//false
-//NOTE ejecutancion de funciones
-//NOTE aqui va ejecutar la primera funcion y luego la segunda
-regresaTrue() && regresaFalse()
-//NOTE aqui va ejecutar solo la primera funcion
-regresaFalse() && regresaTrue()
-console.log('OR')
-console.log(true || false);//true
-console.log(false || false);//false
-//NOTE aqui solo ejecuta la primera funcion
-console.log(regresaTrue() || regresaFalse());//true
-console.log(regresaFalse() || regresaTrue());//true
+//pregunta si la primera evaluacion tiene valor
+//pasa a la siguiente preguntando si tiene valor 
+//si la tercera tiene valor
+//entonces toma ese valor
+//en conclusion toma el ultimo valor
+const a1 = true && 'hola mundo' && 150;
+console.log({ a1 });//150
+//aqui no evalua las demas condiciones
+//nada porque la primera me arroja false
+const a2 = false && 'hola mundo' && 150;
+console.log({ a2 });//false
+const a3 = 'hola' && 'mundo';
+console.log({ a3 });//mundo
+const a4 = soyFalso || 'Ya no soy falso';
+console.log({ a4 });//Ya no soy falso
+const a5 = soyFalso || soyUndefined || soyNull || 'Ya no soy false de nuevo' || true;
+console.log({ a5 });//Ya no soy false de nuevo
+const a6 = soyFalso || soyUndefined || regresaTrue() || 'Ya no soy false de nuevo' || true;
+console.log({ a6 });//true
+
