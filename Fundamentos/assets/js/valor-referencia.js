0540f688e689dae96fa0eea7ad53855057627fd5
dev <46026488+EreGitHub@users.noreply.github.com>
2022-06-21 00:17:21 -0400

    Valor, referencia y romper la referencia

---
+++ Fundamentos/assets/js/valor-referencia.js
@@ -0,0 +1,35 @@
+let a = 10;
+let b = a;
+a = 30;
+console.log({ a, b });
+
+//NOTE pasando valores por referencia
+let juan = { nombre: 'Juan' };
+//let ana = juan;
+//NOTE operador spread
+let ana = { ...juan };//NOTE cambiando el valor de la referencia
+ana.nombre = 'Ana';
+console.log({ juan, ana });//juan y ana tienen el mismo valor
+
+//const cambiaNombre = (persona) => {
+const cambiaNombre = ({ ...persona }) => {
+    persona.nombre = 'Tony'
+    return persona;
+}
+let peter = { nombre: 'Peter' };
+let tony = cambiaNombre(peter);
+console.log({ peter, tony });//peter y tony tienen el mismo valor
+
+//en arreglos
+const frutas = ['manzana', 'pera', 'uva'];
+
+console.time('slice');
+const otrasFrutas = frutas.slice();//otra manera de romper la relacion
+console.timeEnd('slice');
+
+console.time('spread');
+const otrasFrutas2 = [...frutas];//rompo la relacion
+console.timeEnd('spread');
+
+otrasFrutas.push('naranja');
+console.table({ frutas, otrasFrutas2 })
