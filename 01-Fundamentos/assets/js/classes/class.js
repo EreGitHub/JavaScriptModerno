151b604230ccd0c7cdbf3b89c5e507b3cba929bf
dev <46026488+EreGitHub@users.noreply.github.com>
2022-06-27 22:56:31 -0400

    metodos en las clases

--- 01-Fundamentos/assets/js/classes/class.js
+++ 01-Fundamentos/assets/js/classes/class.js
@@ -3,15 +3,23 @@ class Persona {
     codigo = '';
     frase = '';
     constructor(nombre = 'sin nombre', codigo = 'sin codigo', frase = 'sin frase') {
-        console.log('hola!')
-        //codigo = 'ABC'//error por el modo estricto
-        if (!nombre) throw Error('El nombre es obligatorio');
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase = frase;
     }
+
+    quienSoy() {
+        console.log(`soy ${this.nombre} y mi identidad es ${this.codigo}`);
+    }
+    miFrase() {
+        this.quienSoy();
+        console.log(`${this.codigo} dice: ${this.frase}`);
+    }
 }
 const spiderman = new Persona('Peter Parker', 'SPIDER', 'soy tu vecino');
-console.log(spiderman);
 const iroman = new Persona('Tony stark', 'iroman', 'soy tu vecino');
-console.log(iroman);
+
+spiderman.quienSoy();
+iroman.quienSoy();
+spiderman.miFrase();
+iroman.miFrase();
