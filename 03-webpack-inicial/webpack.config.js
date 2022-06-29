d74147df443b63a997177b2d62ad46630612b85f
dev <46026488+EreGitHub@users.noreply.github.com>
2022-06-28 23:46:01 -0400

    HTML Loader

--- 03-webpack-inicial/webpack.config.js
+++ 03-webpack-inicial/webpack.config.js
@@ -1,8 +1,30 @@
+const HtmlWebpack = require('html-webpack-plugin');
+const { Template } = require('webpack');
+
 module.exports = {
     mode: 'development',
+    output: {//limpia los archivos de la salida
+        clean: true
+    },
     module: {
-        rules: []
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false
+                }
+            }
+        ]
     },
     optimization: {},
-    plugins: []
+    plugins: [
+        new HtmlWebpack({
+            title: 'mi Webpack App',
+            //defino el nombre de salida del archivo
+            // filename: 'index.html',
+            //cual es el archivo que se va a usar como plantilla
+            template: './src/index.html'
+        })
+    ]
 }
