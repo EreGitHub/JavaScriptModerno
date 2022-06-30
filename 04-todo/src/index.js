0af4e4d4c231ecb8b1669f2403a55a15a80a842d
dev <46026488+EreGitHub@users.noreply.github.com>
2022-06-30 09:51:01 -0400

    todoList-class

--- 04-todo/src/index.js
+++ 04-todo/src/index.js
@@ -1,5 +1,9 @@
 import './styles.css';
-import { Todo } from './classes/todo.class';
+import { Todo, TodoList } from './classes';
 
-const tarea = new Todo('Aprender JavaScript');
-console.log(tarea);
+const todoList = new TodoList();
+const tarea1 = new Todo('Aprender JavaScript');
+const tarea2 = new Todo('comprar pc');
+todoList.nuevoTodo(tarea1);
+todoList.nuevoTodo(tarea2);
+console.log(todoList);
