747385c6fbe96a020ae856c9b087afab90e8af08
dev <46026488+EreGitHub@users.noreply.github.com>
2022-06-30 10:17:33 -0400

    construir las tareas en el Html

--- 04-todo/src/index.js
+++ 04-todo/src/index.js
@@ -1,9 +1,9 @@
 import './styles.css';
 import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
 
 const todoList = new TodoList();
-const tarea1 = new Todo('Aprender JavaScript');
-const tarea2 = new Todo('comprar pc');
-todoList.nuevoTodo(tarea1);
-todoList.nuevoTodo(tarea2);
+const tarea = new Todo('Aprender JavaScript!!');
+todoList.nuevoTodo(tarea);
 console.log(todoList);
+crearTodoHtml(tarea);
--- 04-todo/src/js/componentes.js
+++ 04-todo/src/js/componentes.js
@@ -1,12 +1,20 @@
-import '../css/componentes.css';
-// import webpacklogo from '../assets/img/webpack-logo.png'
+//referencias
+const divTodoList = document.querySelector('.todo-list');
 
-export const saludar = (nombre) => {
-    console.log('creando etiqueta h1');
-    const h1 = document.createElement('h1');
-    h1.innerText = `Hola ${nombre}!!`;
-    document.body.append(h1);
-    // const img = document.createElement('img');
-    // img.src = webpacklogo;
-    // document.body.append(img);
+export const crearTodoHtml = (todo) => {
+    const htmlTodo = `
+    <li class="${(todo.completado) ? 'completed' : ''}" data-id="${todo.id}">
+        <div class="view">
+            <input class="toggle" type="checkbox" ${(todo.completado) ? 'checked' : ''}>
+            <label>${todo.tarea}</label>
+            <button class="destroy"></button>
+        </div>
+        <input class="edit" value="Create a TodoMVC template">
+    </li>`;
+    //crearmos el div porque me permite crear con todo sus nodos
+    //y al final extraemos el li
+    const div = document.createElement('div');
+    div.innerHTML = htmlTodo;
+    divTodoList.append(div.firstElementChild);
+    return div.firstElementChild;
 }
