0a4d6a05198495d23b4fead8085ef5d41e50acd6
dev <46026488+EreGitHub@users.noreply.github.com>
2022-07-01 12:20:54 -0400

    Promise.all

--- 05-promesas-callbacks/src/index.js
+++ 05-promesas-callbacks/src/index.js
@@ -1,22 +1,9 @@
-import { buscarHeroe as buscarHereoCallback } from './js/callbacks'
 import { buscarHeroe } from './js/promesas'
 import './styles.css';
 
 const heroeId1 = 'capi';
 const heroeId2 = 'hulk';
-buscarHeroe(heroeId1)
-    .then(hereo => {
-        console.log(`enviando a la mision a ${hereo.nombre}`);
-    });
+Promise.all([true, 'hola', 123]).then(console.log);
+Promise.all([buscarHeroe(heroeId1), buscarHeroe(heroeId2)])
+    .then(([heroe]) => console.log(heroe));
 console.log('fin de programa');
-
-// buscarHeroe(heroeId1, (err, heroe1) => {
-//     if (err) return console.error(err);
-//     buscarHeroe(heroeId2, (err, heroe2) => {
-//         if (err) return console.error(err);
-//         buscarHeroe(heroeId2, (err, heroe3) => {
-//             if (err) return console.error(err);
-//             console.log(`enviando a ${heroe1.nombre} y ${heroe2.nombre}`);
-//         });
-//     });
-// });
