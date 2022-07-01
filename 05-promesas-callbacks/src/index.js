809a8fbb1daa0d5297d8de1a6e9fd7844d0ef824
dev <46026488+EreGitHub@users.noreply.github.com>
2022-07-01 10:13:33 -0400

    CallBack Hell

--- 05-promesas-callbacks/src/index.js
+++ 05-promesas-callbacks/src/index.js
@@ -1,10 +1,15 @@
 import { buscarHeroe } from './js/callbacks'
 import './styles.css';
 
-const heroeId = 'capi';
-buscarHeroe(heroeId, (err, heroe) => {
-    if (err)
-        console.error(err);
-    else
-        console.log(heroe)
+const heroeId1 = 'capi';
+const heroeId2 = 'hulk';
+buscarHeroe(heroeId1, (err, heroe1) => {
+    if (err) return console.error(err);
+    buscarHeroe(heroeId2, (err, heroe2) => {
+        if (err) return console.error(err);
+        buscarHeroe(heroeId2, (err, heroe3) => {
+            if (err) return console.error(err);
+            console.log(`enviando a ${heroe1.nombre} y ${heroe2.nombre}`);
+        });
+    });
 });
