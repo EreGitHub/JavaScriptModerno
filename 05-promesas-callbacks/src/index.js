8e2aa25c998b45b04127d9f98fe6c72f8f64e09c
dev <46026488+EreGitHub@users.noreply.github.com>
2022-07-01 13:21:18 -0400

    promise.catch

--- 05-promesas-callbacks/src/index.js
+++ 05-promesas-callbacks/src/index.js
@@ -3,7 +3,8 @@ import './styles.css';
 
 const heroeId1 = 'capi';
 const heroeId2 = 'hulk';
-Promise.all([true, 'hola', 123]).then(console.log);
 Promise.all([buscarHeroe(heroeId1), buscarHeroe(heroeId2)])
-    .then(([heroe]) => console.log(heroe));
+    .then(([heroe]) => console.log(heroe))
+    .catch(console.error)
+    .finally(() => console.log('finalizado el promise.all'));
 console.log('fin de programa');
